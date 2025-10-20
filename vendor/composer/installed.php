334bf4128837af8fe214c4b537dbf82b8a99088e
Tenghuy <lytenghuy8@gmail.com>
2025-10-20 14:00:19 +0700

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'laravel/laravel',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => NULL,
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '502fec163a8f51c18d4c9cfe64348901948abc91',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -431,9 +431,9 @@
             'dev_requirement' => false,
         ),
         'laravel/laravel' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => NULL,
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '502fec163a8f51c18d4c9cfe64348901948abc91',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
