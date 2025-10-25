d7e2a908a2421f40ab2a28257c93b5e19f26d30c
Tenghuy <lytenghuy8@gmail.com>
2025-10-25 11:40:20 +0700

    update share

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -6,11 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
     <meta name="author" content="PayWay">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta property="og:title" content="The Bible Society in Cambodia" />
-    <meta property="og:description" content="The Bible Society in Cambodia" />
     <meta property="og:image" content="{{ asset('assets/images/logo.svg') }}" />
-    <meta property="og:url" content="{{ url()->current() }}" />
-    <meta property="og:type" content="website" />
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
